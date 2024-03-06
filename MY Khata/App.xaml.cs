41d85ceed7f924921e0f89820a478b8a5fe99d79
raushan <raushanc107@gmail.com>
2024-03-06 22:27:51 +0530

    initial code

---
+++ MY Khata/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MY_Khata;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MY Khata/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MY_Khata;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
